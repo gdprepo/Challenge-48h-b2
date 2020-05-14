1365df44ad279f983d5145480a7d46caba0ee01f
dgabin <depairegabin@macbook-pro-de-macbook-1.home>
2020-05-14 15:25:26 +0200

    products img description

--- site/resources/views/acheter/index.blade.php
+++ site/resources/views/acheter/index.blade.php
@@ -2,14 +2,15 @@
 
 @section('content')
 
-    <div class="container" style="width:100%">
+    <div class="container" style="width:100%;">
         <div class="row">
         @foreach($products as $product)
-        <div class="col-sm">
+        <div class="col-sm" style="margin-top: 100px">
             <div class="card" style="width: 100%; margin-top: 50px">
-                <img style="" src="https://i0.wp.com/www.blablahightech.fr/wp-content/uploads/2016/06/maxresdefault.jpg" class="card-img-top" alt="...">
+                <img style="height: 250px" src="{{$product->image}}" class="card-img-top img-thumbnail" alt="...">
                 <div class="card-body">
                     <p class="card-text">{{$product->title}}</p>
+                    <p>{{$product->description}}</p>
                 </div>
             </div>
         </div>
