ec7c9f0bfac2253a09ea498f9437f84da572d783
dgabin <depairegabin@macbook-pro-de-macbook-1.home>
2020-05-14 11:44:43 +0200

    card

--- site/resources/views/welcome.blade.php
+++ site/resources/views/welcome.blade.php
@@ -57,8 +57,8 @@
 
             <div class="container">
 
-                <div class="card" style="margin-right: 30px;width: 50%;">
-                    <img src="https://i0.wp.com/www.blablahightech.fr/wp-content/uploads/2016/06/maxresdefault.jpg" class="card-img-top" alt="...">
+                <div class="card" style="margin-left: 10%; width: 80%; margin-top: 50px">
+                    <img style="height: 400px" src="https://i0.wp.com/www.blablahightech.fr/wp-content/uploads/2016/06/maxresdefault.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
