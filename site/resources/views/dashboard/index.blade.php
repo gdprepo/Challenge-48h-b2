a95dad7b5d44c9cde8609c37a346d094f9dea563
dgabin <depairegabin@macbook-pro-de-macbook-1.home>
2020-05-14 15:31:49 +0200

    dashboard role

---
+++ site/resources/views/dashboard/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="{{ url('/') }}">Skate Bordeaux</a>
+  <div class="top-right links">
+    @auth
+        <a href="{{ url('/home') }}">Home</a>
+        <a href="{{ route('logout') }}">Disconnect</a>
+    @else
+        <a href="{{ route('login') }}">Login</a>
+
+        @if (Route::has('register'))
+            <a href="{{ route('register') }}">Register</a>
+        @endif
+    @endauth
+  </div>
+</nav>
+@section('content')
+<div class="container">
+    <div style="margin-top: 100px" class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Admin</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
